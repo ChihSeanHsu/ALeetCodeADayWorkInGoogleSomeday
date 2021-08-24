5e642e7614d0fb1b383ea7fd19d2de3b4dd53d1a
Chih Sean Hsu <s8901489@gmail.com>
2021-08-24 09:46:40 +0800

    Update solution.py

--- HashMap/13. Roman to Integer/solution.py
+++ HashMap/13. Roman to Integer/solution.py
@@ -27,3 +27,33 @@ class Solution:
                 idx -= 1
         return result
         
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_mapping = {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10,
+            "XL": 40,
+            "L": 50,
+            "XC": 90,
+            "C": 100,
+            "CD": 400,
+            "D": 500,
+            "CM": 900,
+            "M": 1000,
+        }
+        
+        result, prev = 0, 0
+        idx = len(s) - 1
+        while idx != -1:
+            next_num = roman_mapping[s[idx]]
+            if next_num < prev:
+                result -= next_num
+            else:
+                result += next_num
+            prev = next_num
+            idx -= 1
+        return result
+        
