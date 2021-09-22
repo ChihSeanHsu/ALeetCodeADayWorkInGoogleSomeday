354022a64a2e961b918fb3b6b4121aabf0ae8256
Chih Sean Hsu <s8901489@gmail.com>
2021-09-22 22:17:11 +0800

    Update solution.py

--- Two Pointers/141. Linked List Cycle/solution.py
+++ Two Pointers/141. Linked List Cycle/solution.py
@@ -6,6 +6,7 @@
 
 # we can use hash map to solve this problem, but there is a condition to usse constant memory.
 
+# use slow(move one node a time) and fast(move two node a time) pointer, they will meet if there is a loop.
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         slow = fast = head
