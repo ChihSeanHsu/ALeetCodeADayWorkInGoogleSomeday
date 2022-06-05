2dfae799a9687342a2f1ee5e1c0f4881512ab8e7
Chih Sean Hsu <s8901489@gmail.com>
2022-06-05 19:39:20 +0800

    Update solution.py

--- Heap/295. Find Median from Data Stream/solution.py
+++ Heap/295. Find Median from Data Stream/solution.py
@@ -17,7 +17,41 @@ class MedianFinder:
         else:
             return (self.stack[mid] + self.stack[mid - 1]) / 2
         
-        
+
+"""
+left: max_heap
+right: min_heap
+
+
+add: 1
+=> right [1] => pop 1
+=> left [-1]
+left [-1]
+right []
+median: 1
+
+add: 5
+=> left [-5, -1] => pop 5(-5)
+=> right [5]
+left [-1]
+right [5]
+meidan: (1 + 5) / 2
+
+add: 3
+=> right [3, 5] => pop 3
+=> left [-3, -1]
+left [-3, -1]
+right [5]
+median: 3
+
+add: 4
+=> left [-4, -3, -1] => pop 4(-4)
+=> right [4, 5]
+left [-3, -1]
+right [4, 5]
+median: (3 + 4) / 2
+
+"""
 class MedianFinder:
 
     def __init__(self):
